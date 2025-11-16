e5904f7b6da32ee59f6e2c1ece7ae034051f3f98
Imad Dar <imaddar@icloud.com>
2025-11-16 03:06:21 -0700

    logging updated, starting move to tracing

--- src/startup.rs
+++ src/startup.rs
@@ -1,11 +1,10 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::web::Data;
 use actix_web::{App, HttpServer, web};
-use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
-
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
