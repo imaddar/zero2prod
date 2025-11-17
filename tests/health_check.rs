154f366ad26f8517f0fcca1c4e77657a8f06d9d7
Imad Dar <imaddar@icloud.com>
2025-11-17 09:57:58 -0700

    finished tracing, added secrecy

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,4 @@
+use secrecy::ExposeSecret;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -121,7 +122,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+    let mut connection = PgConnection::connect(&config.connection_string_without_db().expose_secret())
         .await
         .expect("Failed to connect to Postgres");
     connection
@@ -129,7 +130,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .await
         .expect("Failed to create database");
 
-    let connection_pool = PgPool::connect(&config.connection_string())
+    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres");
     sqlx::migrate!("./migrations")
