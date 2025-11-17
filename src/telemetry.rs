ba99a85c998adb60ab5569d71390317b37f2c512
Imad Dar <imaddar@icloud.com>
2025-11-17 00:03:55 -0700

    added logging to our tests

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -2,15 +2,19 @@ use std::ops::Sub;
 
 use tokio::sync;
 use tracing::subscriber::set_global_default;
+use tracing_subscriber::fmt::MakeWriter;
 use tracing::Subscriber;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 
-pub fn get_subscriber(
+pub fn get_subscriber<Sink>(
     name: String,
-    env_filter: String
-) -> impl Subscriber + Send + Sync {
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Send + Sync 
+where Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static
+{
     let env_filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new(
